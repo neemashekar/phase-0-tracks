e8f19ad97d43cec00f8834903b0ceb143d2b5a33
Neema Shekar <neema.shekar@snc.edu>
2017-07-30 17:01:25 -0500

    adding method detective

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,52 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# "iNvEsTiGaTiOn".<???>
+puts "iNvEsTiGaTiOn".swapcase
 # => “InVeStIgAtIoN”
 
-# "zom".<???>
+puts "zom".insert(1, 'o')
 # => “zoom”
 
-# "enhance".<???>
+enhance = "enhance"
+enhance.prepend("    ")
+enhance.concat("    ")
+puts enhance
 # => "    enhance    "
 
-# "Stop! You’re under arrest!".<???>
+puts "Stop! You’re under arrest!".upcase
 # => "STOP! YOU’RE UNDER ARREST!"
 
-# "the usual".<???>
+puts "the usual".concat(" suspects")
 #=> "the usual suspects"
 
-# " suspects".<???>
+puts " suspects".prepend("the usual")
 # => "the usual suspects"
 
-# "The case of the disappearing last letter".<???>
+strL = "The case of the disappearing last letter"
+strlenIndex_Last = strL.length - 1
+puts strL.slice(0...strlenIndex_Last)
 # => "The case of the disappearing last lette"
 
-# "The mystery of the missing first letter".<???>
+strF = "The mystery of the missing first letter"
+strlenIndex_First = strF.length
+puts strF.slice(1...strlenIndex_First)
 # => "he mystery of the missing first letter"
 
-# "Elementary,    my   dear        Watson!".<???>
+stringStrip = "Elementary,    my   dear        Watson!"
+strStripLen = stringStrip.length
+S1 = stringStrip.slice(0...12)
+S2 = stringStrip.slice(15...18)
+S3 = stringStrip.slice(20...25)
+S4 = stringStrip.slice(32...39)
+puts S1 + S2 + S3 + S4
 # => "Elementary, my dear Watson!"
 
-# "z".<???>
+"z".each_byte do |byte|
+   puts byte
+end
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
+aCount = "How many times does the letter 'a' appear in this string?".count('a')
+puts aCount
 # => 4
