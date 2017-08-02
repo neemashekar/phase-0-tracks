f2a5ce728d6fecbcd180764c9c322c86d4c2608e
Neema Shekar <neema.shekar@snc.edu>
2017-08-02 15:50:44 -0500

    adding 6.1

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+=begin
+	
+SPECIES --------------------------
+Canis Lupus Familiaris
+
+CHARACTERISTICS ------------------
+Height: 1 foot
+Temperment: Friendly
+Color: Black
+Weight: 20 lbs
+
+BEHAVIOR -------------------------
+Lick
+Wag tail
+Bark
+Pant
+
+	
+end
